da29f3d590538600ab5c48648120413060b4f18a
josejuniorpgx <yunipgx@protonmail.com>
2023-06-20 20:24:54 -0600

    Mas cosas de mi applicacion  Ejemplo.  Y una nueva template

--- applications/home/views.py
+++ applications/home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 # Create your views here.
 
 class IndexView(TemplateView):
     template_name = 'home/home.html' #El archivo html que se va a renderizar y esta en templates
+
+class PruebaListView(ListView):
+    template_name = 'home/vista.html'
+    queryset = ['1','2','3','4','5','6','7','8','9','10'] #Lista de numeros que se van a mostrar en la lista
+    context_object_name = 'listaNumeros' #Nombre de la lista que se va a mostrar en el html
