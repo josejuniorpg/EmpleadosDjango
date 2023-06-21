71410102bdc222c8c4b2ffb8c6c28fc8cbd2c2c2
josejuniorpgx <yunipgx@protonmail.com>
2023-06-20 18:02:59 -0600

    Creo las applicaciones

--- base/settings/local.py
+++ base/settings/local.py
@@ -4,6 +4,18 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = []
 
+INSTALLED_APPS = [ # Aplicaciones instaladas. Y tambien paquetes.
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    #Local Apps
+    'applications.empleado', #En django, las rutas son con . y no con /
+    'applications.departamento',
+]
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
