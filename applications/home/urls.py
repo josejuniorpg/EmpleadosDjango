8a33eb785a20fbb77019e44bb5e58c310243d65a
josejuniorpgx <yunipgx@protonmail.com>
2023-06-22 12:20:08 -0600

    Implementecion del modelo en mi applicacion prueba, y creacion de la tabla prueba

--- applications/home/urls.py
+++ applications/home/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('home/', views.IndexView.as_view()),
     path('vista/', views.PruebaListView.as_view()),
+    path('prueba/', views.ModeloPruebaListView.as_view()),
 ]
--- applications/home/views.py
+++ applications/home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 
+from applications.home.models import Prueba
+
+
 # Create your views here.
 
 class IndexView(TemplateView):
@@ -10,3 +13,8 @@ class PruebaListView(ListView):
     template_name = 'home/vista.html'
     queryset = ['1','2','3','4','5','6','7','8','9','10'] #Lista de numeros que se van a mostrar en la lista
     context_object_name = 'listaNumeros' #Nombre de la lista que se va a mostrar en el html
+
+class ModeloPruebaListView(ListView):
+    template_name = 'home/pruebas.html'
+    model = Prueba
+    context_object_name = 'listaPrueba' #Nombre de la lista que se va a mostrar en el html
