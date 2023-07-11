780c8f607b10d6335728275a40dba9ee3df88a24
josejuniorpgx <yunipgx@protonmail.com>
2023-07-10 23:25:44 -0600

    Configuro para que sea prod

--- example.manage.py
+++ example.manage.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+"""Archivo que muestra como configurar el manage. """
 import os
 import sys
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings.prod')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
